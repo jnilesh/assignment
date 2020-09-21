872f9b237ad9f62de177ac813b10346e29af1cb0
Nilesh Jain <njain.we@gmail.com>
2020-09-21 21:46:09 +0530

    pagination added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,12 +6,15 @@ import Product from './Product'
 function ProductList({id}) {
 
     var [products, setProducts] = useState(null)
+    const [max_page, setMax_page] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1)
 
     useEffect(() => {
-        axios.get(`https://testing.pogo91.com/api/online-store/category/product/?store_prefix=cake-shop&page=1&category_id=${id}`)
+        axios.get(`https://testing.pogo91.com/api/online-store/category/product/?store_prefix=cake-shop&page=${currentPage}&category_id=${id}`)
         .then(response => {
-          console.log(response.data.products);
+          console.log(response);
           setProducts(response.data.products);
+          setMax_page(response.data.num_pages)
         })
         .catch(function (error) {
             // handle error
@@ -19,14 +22,41 @@ function ProductList({id}) {
             alert(error)
           })
         
-    }, [id])
+    }, [id,currentPage])
+
+    const nextPage = () => {
+        if (max_page === currentPage) {
+            alert("No more Pages");
+        }
+        else {
+            setCurrentPage(currentPage + 1)
+        }
+        
+    }
+
+    const previousPage = () => {
+        if (currentPage <= 1) {
+            alert("No previous Pages");
+        }
+        else {
+            setCurrentPage(currentPage - 1)
+        }
+        
+    }
 
     return (
         <div>
             { products && products.map((product)=>(
                 <Product product={product} />
             ))}
+
+            <div className="product__pagination">
+                <button onClick={previousPage} >Previos</button>
+                <button onClick={nextPage} >Next</button>
+                
+            </div>
         </div>
+
     )
 }
 
